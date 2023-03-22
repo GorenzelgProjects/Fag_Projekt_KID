a7d812c63930d8fe801820f2163124f48cf7cf44
logicalrobotic <52610662+logicalrobotic@users.noreply.github.com>
2023-03-22 17:33:38 +0100

    Updated added functions
    Module "read_epochs_eeglab" added
    Function "get_epoch_data" added

--- raw_import.py
+++ raw_import.py
@@ -1,8 +1,8 @@
 import numpy as np
-from mne.io import read_raw_eeglab
+from mne.io import read_raw_eeglab, read_epochs_eeglab
 import matplotlib.pyplot as plt
 import os
-
+import pandas as pd
 
 #PATH = os.path.dirname(os.path.abspath(__file__)) # save this script in same directory as EEG folder
 
@@ -15,6 +15,7 @@ subject = 1 # 1-40
 class EEG():
     
     def __init__(self):
+        self.subject_n = 40
         self.E = 33 # 0-32 electrodes
         self.N = 699392 # number of data points
         self.DT = 0.0009765625 # time-period between each data point
@@ -31,10 +32,20 @@ class EEG():
             x.append(raws[i][1])
             y.append(raws[i][0][0])
 
-        raw_matrix = np.dstack((x,y)) 
+        raw_matrix = np.dstack((x,y))
 
         return raw_matrix #returns 3d array (E,N,(x,y))
 
+
+    def get_epoch_data(self,subject=1, folder="N170 Raw Data and Scripts Only", file="_N170.set"):
+        filename = self.PATH + '/' + folder + '/' + str(subject) + '/' + str(subject)+file
+
+        epoch_data = read_epochs_eeglab(filename)
+
+        for i, item in enumerate(epoch_data):
+            print(item[0,1000])
+
+        #print(epoch_data)
 
     def preprocess(self):
         return 0
@@ -57,31 +68,66 @@ class EEG():
 
     def plotting(self):
         return 0
+
+    def data_split(self, data):
+        x, y = np.rollaxis(data, axis=2)
+        return x,y
     
+    def data_to_csv(self, data, filename="raw_data"):
+        full_filename_1 = self.PATH + "/" + filename + "_x.csv"
+        full_filename_2 = self.PATH + "/" + filename + "_y.csv"
+
+        x,y = self.data_split(data)
+
+        pd.DataFrame(x).to_csv(full_filename_1)
+        pd.DataFrame(y).to_csv(full_filename_2)
+        print("data saved as csv")
+
+    def electrode_avg(self, data):
+        x,y = self.data_split(data)
+
+        x_mean = np.mean(x, axis=0)
+        y_mean = np.mean(y, axis=0)
+
+        return x_mean, y_mean
     
 
 eeg = EEG()
-raw_matrix = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170.set")
-print(raw_matrix.shape[:])
+raw_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170.set")
+raw_x_mean, raw_y_mean = eeg.electrode_avg(raw_data)
 
-norm = eeg.mm_normalize(raw_matrix)
+shifted_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted.set")
+shifted_x_mean, shifted_y_mean = eeg.electrode_avg(shifted_data)
 
-print(norm.shape)
+shifted_ds_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds.set")
+shifted_ds_x_mean, shifted_ds_y_mean = eeg.electrode_avg(shifted_ds_data)
+
+shifted_ds_reref_ucbip_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds_reref_ucbip.set")
+shifted_ds_reref_ucbip_x_mean, shifted_ds_reref_ucbip_y_mean = eeg.electrode_avg(shifted_ds_reref_ucbip_data)
+
+shifted_ds_reref_ucbip_hpfilt_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds_reref_ucbip_hpfilt.set")
+shifted_ds_reref_ucbip_hpfilt_x_mean, shifted_ds_reref_ucbip_hpfilt_y_mean = eeg.electrode_avg(shifted_ds_reref_ucbip_hpfilt_data)
 
 
-#print(raw_matrix[0,:100,0])
 
 
-#print(raw_matrix[0][1000][0])
-#print(raw_matrix[0][1000][1])
-fig, axs = plt.subplots(3,1)
+#eeg.get_epoch_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds_reref_ucbip_hpfilt_ica_corr_cbip_elist_bins_epoch_interp.set")
+
+#print(final_data.shape[:])
+#norm = eeg.mm_normalize(raw_matrix)
+
+
+
+#fig, axs = plt.subplots(3,1)
     #print(i)
 
-#for i in range(100):
-    #print(raw_matrix[0][i][0])
-    #print(raw_matrix[0][i][1])
 
-axs[0].plot(raw_matrix[0,:,0], raw_matrix[0,:,1], color="b")
+axs[0].plot(raw_data[0,:,0], raw_data[0,:,1], color="b")
+#axs[0].plot(raw_x_mean, raw_y_mean, color="r")
+
+axs[1].plot(shifted_ds_reref_ucbip_data[0,:,0], shifted_ds_reref_ucbip_data[0,:,1], color="b")
+#axs[1].plot(shifted_ds_reref_ucbip_x_mean, shifted_ds_reref_ucbip_y_mean, color="r")
+
+axs[2].plot(shifted_ds_reref_ucbip_hpfilt_data[0,:,0], shifted_ds_reref_ucbip_hpfilt_data[0,:,1], color="b")
 plt.show()
-
 
