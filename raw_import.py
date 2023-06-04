bb839306b20979db9c087b5112dec57617bd33d8
Rasmus2001 <RasmusSS2001@gmail.com>
2023-06-04 16:36:53 +0200

    Update raw_import
    A section has been added to the data analysis, in addition some comments have been added.

--- raw_import.py
+++ raw_import.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
+'''The purpose of this class is to gather the right data so that we can get the data thourg the rigth preprocesses. Which includes shifting, 
+Downsampling, Re-reference, bipolar HEOG channel and high-pass filter.'''
+
 #PATH = os.path.dirname(os.path.abspath(__file__)) # save this script in same directory as EEG folder
 
 #E = 33 # 0-32 electrodes
@@ -94,19 +97,19 @@ class EEG():
 
 eeg = EEG()
 raw_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170.set")
-raw_x_mean, raw_y_mean = eeg.electrode_avg(raw_data)
+raw_x_mean, raw_y_mean = eeg.electrode_avg(raw_data)        #Get the means of the x and y's in raw_data
 
 shifted_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted.set")
-shifted_x_mean, shifted_y_mean = eeg.electrode_avg(shifted_data)
+shifted_x_mean, shifted_y_mean = eeg.electrode_avg(shifted_data)        #Do the shifting in the data.
 
 shifted_ds_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds.set")
-shifted_ds_x_mean, shifted_ds_y_mean = eeg.electrode_avg(shifted_ds_data)
+shifted_ds_x_mean, shifted_ds_y_mean = eeg.electrode_avg(shifted_ds_data)       #Downsampling of the data 1024 Hz to 256 Hz to speed data processing
 
 shifted_ds_reref_ucbip_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds_reref_ucbip.set")
-shifted_ds_reref_ucbip_x_mean, shifted_ds_reref_ucbip_y_mean = eeg.electrode_avg(shifted_ds_reref_ucbip_data)
+shifted_ds_reref_ucbip_x_mean, shifted_ds_reref_ucbip_y_mean = eeg.electrode_avg(shifted_ds_reref_ucbip_data)       #Re-reference the data to the average of all 33 EEG sites and create a bipolar HEOG channel.
 
 shifted_ds_reref_ucbip_hpfilt_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170_shifted_ds_reref_ucbip_hpfilt.set")
-shifted_ds_reref_ucbip_hpfilt_x_mean, shifted_ds_reref_ucbip_hpfilt_y_mean = eeg.electrode_avg(shifted_ds_reref_ucbip_hpfilt_data)
+shifted_ds_reref_ucbip_hpfilt_x_mean, shifted_ds_reref_ucbip_hpfilt_y_mean = eeg.electrode_avg(shifted_ds_reref_ucbip_hpfilt_data)      #Remove the DC offsets and apply a high-pass filte
 
 
 
@@ -122,7 +125,7 @@ shifted_ds_reref_ucbip_hpfilt_x_mean, shifted_ds_reref_ucbip_hpfilt_y_mean = eeg
     #print(i)
 
 
-axs[0].plot(raw_data[0,:,0], raw_data[0,:,1], color="b")
+axs[0].plot(raw_data[0,:,0], raw_data[0,:,1], color="b")           #Plots the values
 #axs[0].plot(raw_x_mean, raw_y_mean, color="r")
 
 axs[1].plot(shifted_ds_reref_ucbip_data[0,:,0], shifted_ds_reref_ucbip_data[0,:,1], color="b")
@@ -131,3 +134,53 @@ axs[1].plot(shifted_ds_reref_ucbip_data[0,:,0], shifted_ds_reref_ucbip_data[0,:,
 axs[2].plot(shifted_ds_reref_ucbip_hpfilt_data[0,:,0], shifted_ds_reref_ucbip_hpfilt_data[0,:,1], color="b")
 plt.show()
 
+
+'''Some values and list for further statistics e.g the mean of brain waves for each individual.'''
+list_of_means = []
+male = [0,4,5,11,12,13,14,18,20,23,25,26,31,34,39]
+female = [1,2,3,6,7,8,9,10,15,16,17,19,21,22,24,27,28,29,30,32,33,35,36,37,38]
+for i in range(1,41): #Finds the mean for every subject.
+    subject = i # 1-40
+    eeg = EEG()
+    raw_data = eeg.get_raw_data(subject=subject, folder="N170 Raw Data and Scripts Only", file="_N170.set")
+    raw_x_mean, raw_y_mean = eeg.electrode_avg(raw_data)
+
+    list_of_means.append(raw_y_mean)
+
+    #print(raw_y_mean)
+    #print(len(raw_y_mean))
+sub_mean = np.mean(list_of_means, axis=1)
+sub_mean_total = np.mean(sub_mean)
+sub_max = np.amax(list_of_means, axis=1)
+sub_max_total = np.amax(sub_max)
+sub_min = np.amin(list_of_means, axis=1)
+sub_min_total = np.amin(sub_min)
+male_list = [list_of_means[i] for i in male]
+female_list = [list_of_means[i] for i in female]
+male_list_max = np.amax(male_list)
+female_list_max = np.amax(female_list)
+male_list_min = np.amin(male_list)
+female_list_min = np.amin(female_list)
+sub_mean_male = np.mean(male_list, axis=1)
+sub_mean_female = np.mean(female_list, axis=1)
+sub_mean_male_total = np.mean(sub_mean_male)
+sub_mean_female_total = np.mean(sub_mean_female)
+#print(list_of_means)
+print(sub_mean)
+print(sub_mean_total)
+print("-------------------------------------------------")
+print(sub_max)
+print(sub_min)
+print(sub_max_total)
+print(sub_min_total)
+print("-------------------------------------------------")
+print(sub_mean_male)
+print(sub_mean_female)
+print("-------------------------------------------------")
+print(sub_mean_male_total)
+print(sub_mean_female_total)
+print("-------------------------------------------------")
+print(male_list_max)
+print(male_list_min)
+print(female_list_max)
+print(female_list_min)
