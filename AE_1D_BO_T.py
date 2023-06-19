9e331beed1c3cc74191d69888b5d2e75513bdd53
GorenzelgProjects <98017958+GorenzelgProjects@users.noreply.github.com>
2023-06-19 22:54:55 +0200

    Update AE_1D_BO_T.py
    Added comments

--- AE_1D_BO_T.py
+++ AE_1D_BO_T.py
@@ -14,10 +14,16 @@ import torch.nn.functional as F
 import torch.optim as optim
 import GPyOpt
 
-path = "./AET_plots_BO"
+'''This file handles the Bayesian Optimization using GPyOpt for both the CAE and TECAE. Most of the code is the objective function.
+Remember to change the settings needed for it try train the CAE vs. TECAE.
+'''
 
-paradigm = False
-reject = True
+'''Hyper parameter settings'''
+
+path = "./AE_plots_BO"       #The plots will be saved at this relative path in a folder with the given name.
+
+paradigm = False             #Set to true if we want to train over paradigms
+reject = False               #Set to true if we want to rejects "bad" subject/datapoints
 
 kernel = 3
 layers = 2
@@ -29,11 +35,11 @@ padding_p = 0
 #criterion = nn.L1Loss()
 
 batch_size=32
-n = 12                   # Number of labels
-transfer = 0
+n = 12                          #Number of labels
+transfer = 0                    #Number of transfers when using single train runs.
 
 
-nhead = tuple([4,8,16,32])
+#nhead = tuple([4,8,16,32])
 learning_rate = (1*1e-5,1)
 weight_decay = (1*1e-5,1)
 
@@ -47,14 +53,14 @@ weight_decay = (1*1e-5,1)
 #padding = tuple([0,1,2]) 
 #padding_p = tuple([0,0]) 
 
-domain = [{'name': 'nhead', 'type': 'discrete', 'domain': nhead},                               #0         
-          {'name': 'learning_rate', 'type': 'continuous', 'domain': learning_rate},               #1
-          {'name': 'weight_decay', 'type': 'continuous', 'domain': weight_decay},                 #2
-         ]
+#domain = [{'name': 'nhead', 'type': 'discrete', 'domain': nhead},                               #0         
+#          {'name': 'learning_rate', 'type': 'continuous', 'domain': learning_rate},               #1
+#          {'name': 'weight_decay', 'type': 'continuous', 'domain': weight_decay},                 #2
+#         ]
 
-#domain = [{'name': 'learning_rate', 'type': 'continuous', 'domain': learning_rate},               #0
-#          {'name': 'weight_decay', 'type': 'continuous', 'domain': weight_decay},                 #1
- #        ]
+domain = [{'name': 'learning_rate', 'type': 'continuous', 'domain': learning_rate},               #0
+          {'name': 'weight_decay', 'type': 'continuous', 'domain': weight_decay},                 #1
+         ]
 
 train_data, train_labels, test_data, test_labels, avg, avg_labels, avg_test, avg_labels_test = dataload_init(n=12, paradigm=paradigm, reject=reject)
 
@@ -87,12 +93,12 @@ def objective_function(x):
     #    criterion = nn.CrossEntropyLoss()
     
 
-    autoencoder = AET(encoder_list=0,
+    autoencoder = AE(encoder_list=0,
                 decoder_list=decoder_list, 
                 transformer_in=transformer_in,
                 in_channels = in_channels, 
                 out_channels = out_channels, 
-                nhead = int(param[0]), 
+                nhead = 0,#int(param[0]), 
                 layers=int(layers), 
                 kernel = int(kernel), 
                 kernel_p = int(kernel_p), 
@@ -105,12 +111,15 @@ def objective_function(x):
     params_to_optimize = [
     {'params': autoencoder.parameters()},
     ]
+
+                #Optimizer- and loss functions settings. uncomment/comment the setting you want.
+
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     #autoencoder = EEGAutoencoder()                                  #We've merely set these to 
-    criterion = nn.L1Loss()
-    #criterion = nn.MSELoss()
+    #criterion = nn.L1Loss()
+    criterion = nn.MSELoss()
     #optimizer = torch.optim.Adam(autoencoder.parameters(), lr=param[1],weight_decay=param[2])
-    optimizer = torch.optim.SGD(autoencoder.parameters(), lr=param[1],momentum=param[2])
+    optimizer = torch.optim.SGD(autoencoder.parameters(), lr=param[0],momentum=param[1])
 
     autoencoder.to(device)
    
@@ -120,7 +129,7 @@ def objective_function(x):
 
     model = Epoch(autoencoder, device, train_loader, valid_loader, test_loader, avg_dataset, avg_dataset_test, avg_target, avg_test_target, criterion, optimizer, test_dataset, target_labels, n=n, PATH=path, paradigm=paradigm)      
     #model.to(device)
-    model.train(num_epochs=num_epochs, verbose=True) #dataloader, loss_fn, optimizer,n=10))
+    model.train(num_epochs=num_epochs) #dataloader, loss_fn, optimizer,n=10))
 
     test_loss = model.diz_loss['val_loss']
     test_loss = test_loss[-1]
@@ -141,14 +150,14 @@ x_best = opt.X[np.argmin(opt.Y)]
 print(x_best)
 
       
-print("The best parameters obtained:" 
-      + domain[0]["name"] + "=" + str(x_best[0]) + "," 
-      + domain[1]["name"] + "=" + str(x_best[1]) + "," 
-      + domain[2]["name"] + "=" + str(x_best[2]))
-
 #print("The best parameters obtained:" 
 #      + domain[0]["name"] + "=" + str(x_best[0]) + "," 
-#      + domain[1]["name"] + "=" + str(x_best[1]))
+#     + domain[1]["name"] + "=" + str(x_best[1]) + "," 
+#      + domain[2]["name"] + "=" + str(x_best[2]))
+
+print("The best parameters obtained:" 
+      + domain[0]["name"] + "=" + str(x_best[0]) + "," 
+      + domain[1]["name"] + "=" + str(x_best[1]))
 
 try:
     opt.plot_acquisition()
